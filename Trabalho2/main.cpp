c2b77d06797c0738760e471eb14caebee28476b8
unknown <14107687@W1607522.portoalegre.pucrsnet.br>
2015-11-05 20:51:23 -0200

    test3

--- Trabalho2/main.cpp
+++ Trabalho2/main.cpp
@@ -1,9 +1,35 @@
 //http://www.cranems.com/
 //teste2
-
 #include <iostream>
+# include <vector>
 
 using namespace std;
+
+
+class MaquinaVenda{
+private:
+vector<MaquinaVenda> mv;
+
+public:
+MaquinaVenda();
+//void Estoque(int v);
+//void InformarPreço();
+//float LerDinheiro(int  v);
+//float DarTroco(int v);
+
+
+
+};
+
+
+MaquinaVenda::MaquinaVenda(){
+
+}
+//void Estoque(int v){
+
+   // mv.push_back(v);
+//}
+
 
 
 int main()
@@ -11,8 +37,9 @@ int main()
 
    cout << "Maquina de vendas" << endl ;
 
+   vector <MaquinaVenda> maq;
 
-
-
+    //maq.Estoque(5);
+    //cout << maq;
 
 }
